5fc9500a5dbe1b45816ee05b8d00649af391acc7
aalinegf <84750262+aalinegf@users.noreply.github.com>
2022-04-12 20:33:17 -0300

    Add files via upload

---
+++ TCC-As-Iluminadas/controller/VendaController.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const venda = require('../model/Venda');
+
+const router = express.Router();
+
+
+router.post();
+
+
+router.get();
+
+
+router.put();
+
+router.delete();
