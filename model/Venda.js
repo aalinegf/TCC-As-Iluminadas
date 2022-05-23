0d9c7f4a40f41411ba12cf201f703cc5a678b2de
Aline Galvão <84750262+aalinegf@users.noreply.github.com>
2022-05-23 20:40:58 -0300

    ocorrendo mudanças..

--- model/Venda.js
+++ model/Venda.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize')
 
-const connection = require('../database/database')
+const connection = require('../database/database');
+const Carrinho = require('./Carrinho');
+const Cliente = require('./Cliente');
+const Produto = require('./Produto');
 
-const Cliente = require('./Cliente')
 
 const Venda = connection.define('tbl_venda', {
   id: {
@@ -11,22 +13,19 @@ const Venda = connection.define('tbl_venda', {
     primaryKey: true,
     allowNull: false,
   },
-
-  date: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-  id_venda_cliente: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
+  total_price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false 
   },
 })
 
-Venda.belongsTo(Cliente, {
-  constraint: true,
-  foreignKey: 'fk_ven_cli',
-})
 
+  /** CHAVES ESTRANGEIRAS */
+
+
+  Cliente.hasMany(Venda);
+  Produto.hasMany(Venda); 
+  Carrinho.hasMany(Venda);
 
     // Venda.sync({force: true})
 
